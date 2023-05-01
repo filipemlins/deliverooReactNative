5cd361c9c41c414be5309e18145aa070bda716c4
Filipe Maciel Lins <filipemlins@gmail.com>
2023-05-01 20:46:29 -0300

    Update RestaurantScreen.js
    2h:05:32

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,15 +1,16 @@
-import {ScrollView, View, Image, Text, TouchableOpacity} from 'react-native'
+import { ScrollView, View, Image, Text, TouchableOpacity } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { urlFor } from '../sanity'
 import { ArrowLeftIcon } from 'react-native-heroicons/solid';
-import {UserIcon,
+import {
+    UserIcon,
     ChevronDownIcon,
     MagnifyingGlassIcon,
     AdjustmentsVerticalIcon,
     StarIcon,
 } from "react-native-heroicons/solid"
-import {MapPinIcon } from  "react-native-heroicons/outline"
+import { ChevronRightIcon, MapPinIcon } from "react-native-heroicons/outline"
 
 
 import { QuestionMarkCircleIcon } from 'react-native-heroicons/outline';
@@ -17,7 +18,7 @@ import { QuestionMarkCircleIcon } from 'react-native-heroicons/outline';
 const RestaurantScreen = () => {
     const navigation = useNavigation();
     const {
-        params : {
+        params: {
             id,
             imgURL,
             title,
@@ -29,15 +30,15 @@ const RestaurantScreen = () => {
             long,
             lat
         }
-    
+
     } = useRoute();
 
     // console.log(genre)
 
     useLayoutEffect(() => {
-      navigation.setOptions({
-        headerShown: false,
-      })
+        navigation.setOptions({
+            headerShown: false,
+        })
     }, []);
 
     return (
@@ -46,14 +47,14 @@ const RestaurantScreen = () => {
                 <Image
                     source={{
                         uri: urlFor(imgURL).url(),
-                     }}
+                    }}
                     className="w-full h-56 bg-gray-300 p-4"
-                
+
                 />
             </View>
 
             <TouchableOpacity onPress={navigation.goBack} className="absolute top-10 p-2 bg-gray-100 rounded-full">
-                <ArrowLeftIcon size={20} color={"#00CCBB"}/>
+                <ArrowLeftIcon size={20} color={"#00CCBB"} />
             </TouchableOpacity>
 
             <View className='bg-white'>
@@ -61,25 +62,48 @@ const RestaurantScreen = () => {
                     <Text className='text-3xl font-bold'>{title}</Text>
                     <View className='flex-row space-x-2 my-1'>
                         <View className='flex-row items-center space-x-1'>
-                            <StarIcon size={22} color={"#00CCBB"} opacity={0.5}/>
+                            <StarIcon size={22} color={"#00CCBB"} opacity={0.5} />
                             <Text className="text-xs text-gray-500">
                                 <Text className="text-green-500">{rating}</Text> • {genre.title}
                             </Text>
                         </View>
 
                         <View className='flex-row space-x-2 my-1'>
-                        <View className='flex-row items-center space-x-1'>
-                            <MapPinIcon size={22} color="gray" opacity={0.4}/>
-                            <Text className="text-xs text-gray-500">
-                                <Text className="text-green-500">Nearby</Text> • {address}
-                            </Text>
-                        </View>
+                            <View className='flex-row items-center space-x-1'>
+                                <MapPinIcon size={22} color="gray" opacity={0.4} />
+                                <Text className="text-xs text-gray-500">
+                                    <Text className="text-green-500">Nearby</Text> • {address}
+                                </Text>
+                            </View>
                         </View>
 
                     </View>
+                    <Text className='mt-2 pb-4 text-gray-500'>{short_description}</Text>
                 </View>
-            </View>        
-            
+
+                <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
+                    <QuestionMarkCircleIcon size={20} color="gray" opacity={0.6} />
+                    <Text className="pl-2 flex-1 text-md font-bold">
+                        Have a food allergy?
+                    </Text>
+                    <ChevronRightIcon color={"#00CCBB"} />
+                </TouchableOpacity>
+
+            </View>
+
+            <View>
+                <Text className="font-bold px-4 pt-6 mb-3 text-xl">
+                    Menu
+                </Text>
+            </View>
+
+            {/* Dishrows */}
+            { dishes.map( dish => (
+                <DishRow 
+                
+                />
+            ))}
+
 
         </ScrollView>
     )
